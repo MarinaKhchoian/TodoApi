31c123d185ba48610f3f1e2283be29db6ddfe01c
MarinaKhchoian <154251511+MarinaKhchoian@users.noreply.github.com>
2026-01-15 01:15:13 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// ????????? DbContext ? InMemory ????? ?????? (??? ????????)
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseInMemoryDatabase("TodoList"));
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
